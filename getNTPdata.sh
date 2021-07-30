35c8c957ea21f4b7eb8d6faa97dc7a6043c4a5cf
hohno-46466 <hohno.46466@gmail.com>
2021-07-30 19:35:33 +0900

    updated by @hohno_at_kuimc on hohno-MBP-2016B.local

--- getNTPdata.sh
+++ getNTPdata.sh
@@ -2,14 +2,14 @@
 
 # getNTPdata.sh
 
-# Last update: Thu Apr  9 06:34:13 JST 2020 by @hohno_at_kuimc
+# Prev update: Thu Apr  9 06:34:13 JST 2020 by @hohno_at_kuimc
+# Last update: Fri Jul 30 19:33:46 JST 2021 by @hohno_at_kuimc
 
 # Public domain
 
 NTPserver="ntp.nict.jp"
 
-
-gawk 'BEGIN{printf "%c", 35; for(i=0;i<47;i++){printf "%c", 0};fflush()} {}'< /dev/null \
+gawk 'BEGIN{printf "%c", 35; for(i=0;i<47;i++){printf "%c", 0};fflush()} {}' < /dev/null \
 | /usr/bin/nc -w 1 -u ${NTPserver} 123 \
 | ptw hexdump -C \
 | head -3
