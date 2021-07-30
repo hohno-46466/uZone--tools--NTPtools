3981ef7c7d3d39ca1fed3426c70c00f5f4bcc9e1
hohno-46466 <hohno.46466@gmail.com>
2021-07-30 09:27:06 +0900

    updated by @hohno_at_kuimc on hohno-MBP-2016B.local

--- NTPtime2unixTime.sh
+++ NTPtime2unixTime.sh
@@ -1,8 +1,10 @@
 #! /bin/sh
 
 # NTPtime2unixTime.sh
-# Last update: Thu Apr  9 06:34:13 JST 2020 by @hohno_at_kuimc
+
+# Prev update: Thu Apr  9 06:34:13 JST 2020 by @hohno_at_kuimc
+# Last update: Thu Jul 29 23:31:16 JST 2021 by @hohno_at_kuimc
 
 # Public domain
 
-gsed -e 's/ \(3[0-9]\{9\}\) / $((\1 - 2208988800)) /'  -e 's/^/echo "/'  -e 's/$/"/' | sh
+gsed -e 's/\(3[0-9]\{9\}\)/$((\1 - 2208988800))/g'  -e 's/^/echo "/'  -e 's/$/"/' | sh
--- getNTPdata.sh
+++ getNTPdata.sh
@@ -1,23 +1,23 @@
 #! /bin/sh
 
 # getNTPdata.sh
+
 # Last update: Thu Apr  9 06:34:13 JST 2020 by @hohno_at_kuimc
 
 # Public domain
 
 NTPserver="ntp.nict.jp"
 
-# (echo ""; sleep 0.1) \
 
 gawk 'BEGIN{printf "%c", 35; for(i=0;i<47;i++){printf "%c", 0};fflush()} {}'< /dev/null \
 | /usr/bin/nc -w 1 -u ${NTPserver} 123 \
 | ptw hexdump -C \
-| head -3 \
+| head -3
 
 exit
+
+# (echo ""; sleep 0.1)
 
 # | sed -e 's/00*[0-9]0 *//g' -e 's/ *|.*$//g' \
 # | gawk '{printf "%s ",$0; if (NR==3){printf "\n"}; fflush()}'
-
-exit
 
