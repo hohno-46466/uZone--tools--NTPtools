b06c5649c1f65997b67a37a0bef6c3848623f704
hohno-46466 <hohno.46466@gmail.com>
2021-10-03 21:20:26 +0900

    updated by @hohno_at_kuimc on hohno-AX3-2020J

--- getNTPdata.sh
+++ getNTPdata.sh
@@ -1,18 +1,23 @@
 #! /bin/sh
 
 # getNTPdata.sh
-# Last update: Thu Apr  9 06:34:13 JST 2020 by @hohno_at_kuimc
+
+# Prev update: Thu Apr  9 06:34:13 JST 2020 by @hohno_at_kuimc
+# Last update: Thu Jul 29 22:23:52 JST 2021 by @hohno_at_kuimc
 
 # Public domain
 
 NTPserver="ntp.nict.jp"
 
-# (echo ""; sleep 0.1) \
+nc=/bin/nc
+ptw=/home/hohno/bin/ptw
+gawk=/usr/bin/gawk
+head=/usr/bin/head
 
-gawk 'BEGIN{printf "%c", 35; for(i=0;i<47;i++){printf "%c", 0};fflush()} {}'< /dev/null \
-| /usr/bin/nc -w 1 -u ${NTPserver} 123 \
-| ptw hexdump -C \
-| head -3 \
+$gawk 'BEGIN{printf "%c", 35; for(i=0;i<47;i++){printf "%c", 0};fflush()} {}'< /dev/null \
+| $nc -w 1 -u ${NTPserver} 123 \
+| $ptw hexdump -C \
+| $head -3
 
 exit
 
