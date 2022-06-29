35e1e0d5b86c54bab82858b2c2630fe4afcf963f
hohno-46466 <hohno.46466@gmail.com>
2022-06-29 19:16:22 +0900

    updated by @hohno_at_kuimc on hohno-AX3-2020J

--- scripts/getNTPdata.sh
+++ scripts/getNTPdata.sh
@@ -3,17 +3,18 @@
 # getNTPdata.sh
 
 # Prev update: Thu Apr  9 06:34:13 JST 2020 by @hohno_at_kuimc
-# Last update: Fri Jul 30 19:33:46 JST 2021 by @hohno_at_kuimc
+# Prev update: Fri Jul 30 19:33:46 JST 2021 by @hohno_at_kuimc
+# Last update: Wed Jun 29 19:06:48 JST 2022 by @hohno_at_kuimc
 
 # Public domain
 
 NTPserver="ntp.nict.jp"
 
-exit_on_error() { echo "Error: $1 (ErrorCode = $2)"; exit $2; }
+exit_on_error() { err=$1; shift; echo "Error: $@ (ErrorCode = $err)"; exit $err; }
 
-[ "x$(which nc)" = "x" ]   && exit_on_error "Prepare nc command" 9
-[ "x$(which pxw)" = "x" ]  && exit_on_error "Prepare ptw command" 9 
-[ "x$(which gawk)" = "x" ] && exit_on_error "Prepare gawk command" 9 
+[ "x$(which nc)" = "x" ]   && exit_on_error 9 "prepare nc command"
+[ "x$(which pxtw)" = "x" ] && exit_on_error 8 "prepare ptw command"
+[ "x$(which gawk)" = "x" ] && exit_on_error 7 "prepare gawk command"
 
 gawk 'BEGIN{printf "%c", 35; for(i=0;i<47;i++){printf "%c", 0};fflush()} {}' < /dev/null \
 | nc -w 1 -u ${NTPserver} 123 \
