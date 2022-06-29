ceec1c3defe8a8dbbd3de82f1cca52e9bba920b3
hohno-46466 <hohno.46466@gmail.com>
2022-06-29 21:55:53 +0900

    updated by @hohno_at_kuimc on hohno-AX3-2020J

--- scripts/demo629.sh
+++ scripts/demo629.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 NTPserver="ntp.nict.jp"
 gawk 'BEGIN{printf "%c", 35; for(i=0;i<47;i++){printf "%c", 0};fflush()} {}' < /dev/null \
-| nc -w 1 -u ${NTPserver} 123 \
+| ptw nc -w 1 -u ${NTPserver} 123 \
 | ptw hexdump -C \
 | head -3
 
--- scripts/getNTPdata.sh
+++ scripts/getNTPdata.sh
@@ -8,13 +8,14 @@
 
 # Public domain
 
-NTPserver="ntp.nict.jp"
 
 exit_on_error() { err=$1; shift; echo "Error: $@ (ErrorCode = $err)"; exit $err; }
 
 [ "x$(which nc)" = "x" ]   && exit_on_error 9 "prepare nc command"
-[ "x$(which pxtw)" = "x" ] && exit_on_error 8 "prepare ptw command"
+[ "x$(which ptw)" = "x" ]  && exit_on_error 8 "prepare ptw command"
 [ "x$(which gawk)" = "x" ] && exit_on_error 7 "prepare gawk command"
+
+NTPserver=${1-"ntp.nict.jp"}
 
 gawk 'BEGIN{printf "%c", 35; for(i=0;i<47;i++){printf "%c", 0};fflush()} {}' < /dev/null \
 | nc -w 1 -u ${NTPserver} 123 \
