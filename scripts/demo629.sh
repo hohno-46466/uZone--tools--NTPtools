4fa74c87bab30766451e77493ff219b4b72fabc3
hohno-46466 <hohno.46466@gmail.com>
2022-07-06 19:29:53 +0900

    updated by @hohno_at_kuimc on hohno-AX3-2020J

--- scripts/demo629.sh
+++ scripts/demo629.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 NTPserver="ntp.nict.jp"
+nc=nc; [ -x /usr/bin/nc ] && nc=/usr/bin/nc
 gawk 'BEGIN{printf "%c", 35; for(i=0;i<47;i++){printf "%c", 0};fflush()} {}' < /dev/null \
-| ptw nc -w 1 -u ${NTPserver} 123 \
-| ptw hexdump -C \
+| $nc -w 1 -u ${NTPserver} 123 \
+| stdbuf -oL hexdump -C \
 | head -3
 
